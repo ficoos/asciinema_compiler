04f10cd85ac324fce2aa63899eacc4b86ccd6a4c
Saggi Mizrahi <saggi@mizrahi.cc>
2017-11-09 21:40:17 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Asciinema Compiler
+
+This is a small program to compile asciinema recordings to self contained htmls.
+No need to upload to the web in order to share your recording!
+
+# Installation
+
+```
+$ go get -u github.com/ficoos/asciinema_compiler
+```
+
+# How to use
+
+```
+$ asciinema_compiler -in demo.json -out demo.html
+```
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"text/template"
+	"io"
+	"os"
+	"io/ioutil"
+	"flag"
+)
+
+type Ctx struct {
+	Css string
+	Js string
+	Json string
+}
+
+func unsafeReadAll(r io.Reader) string {
+	res, _ := ioutil.ReadAll(r)
+	return string(res)
+}
+
+func unsafeAsset(path string) string {
+	data, err := Asset(path);
+	if err != nil {
+		panic(err)
+	}
+
+	return string(data)
+}
+
+func main() {
+	var inputFile io.Reader
+	var outputFile io.Writer
+	var err error
+
+	inputFilePath := flag.String("in", "", "Input record json file")
+	outputFilePath := flag.String("out", "", "Ouput html file")
+
+	flag.Parse()
+	if *inputFilePath == "" {
+		inputFile = os.Stdin
+	} else {
+		inputFile, err = os.Open(*inputFilePath)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Could not process input file:", err)
+			os.Exit(1)
+		}
+
+		defer inputFile.(*os.File).Close()
+	}
+
+	if *outputFilePath == "" {
+		outputFile = os.Stdout
+	} else {
+		outputFile, err = os.OpenFile(*outputFilePath, os.O_RDWR | os.O_CREATE, 0755);
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Could not process output file:", err)
+			os.Exit(1)
+		}
+
+		defer outputFile.(*os.File).Close()
+	}
+
+	tmpl, err := template.New("asciinema").Parse(unsafeAsset("data/template.html"));
+	if err != nil { panic(err) }
+	ctx := Ctx{
+		Css: unsafeAsset("data/asciinema-player.css"),
+		Js: unsafeAsset("data/asciinema-player.js"),
+		Json: unsafeReadAll(inputFile),
+	}
+	err = tmpl.Execute(outputFile, ctx);
+	if err != nil { panic(err) }
+}
